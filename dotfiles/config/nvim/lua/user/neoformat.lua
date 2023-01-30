e04b36f83cecb9db873e004b888224f976756f74
Carl Waldbieser <cwaldbieser@gmail.com>
2023-01-30 13:00:14 -0500

    Use `yamlfix` for YAML formatting.
    Use `yamlfix` for YAML formatting.

--- dotfiles/config/nvim/lua/user/neoformat.lua
+++ dotfiles/config/nvim/lua/user/neoformat.lua
@@ -4,6 +4,18 @@
 
 vim.g.neoformat_run_all_formatters = 1
 
+-- Uncomment for debugging.
+--vim.g.neoformat_verbose = 1
+
+-- YAML
+vim.g.neoformat_yaml_yamlfix = {
+    exe = 'yamlfix',
+    stdin = 1,
+    args = { '-' }
+}
+vim.g.neoformat_enabled_yaml = {'yamlfix'}
+
+
 -- JSON
 vim.g.neoformat_json_jq = {
     exe = 'jq',
