82315b1acf4465e61d3c81e38a465ff433d1f888
Carl Waldbieser <cwaldbieser@gmail.com>
2023-02-22 10:37:50 -0500

    [nvim] Added SQL linter and formatter.
    [nvim] Added SQL linter and formatter.

--- dotfiles/config/nvim/lua/user/null-ls.lua
+++ dotfiles/config/nvim/lua/user/null-ls.lua
@@ -2,7 +2,6 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.completion.spell,
         null_ls.builtins.formatting.black, -- python formatter
         null_ls.builtins.formatting.autoflake, -- python remove unused imports
@@ -12,6 +11,12 @@ null_ls.setup({
         null_ls.builtins.diagnostics.shellcheck, -- shell script diagnostics
         null_ls.builtins.code_actions.shellcheck, -- shell script code actions
         null_ls.builtins.formatting.shfmt, -- shell script formatting
+        null_ls.builtins.diagnostics.sqlfluff.with({
+            extra_args = { "--dialect", "ansi" }, -- SQL linter
+        }),
+        null_ls.builtins.formatting.sqlfluff.with({
+            extra_args = { "--dialect", "ansi" }, -- SQL formatter
+        }),
     },
 })
 
@@ -19,11 +24,12 @@ null_ls.setup({
 local add_desc = require("user.keymap_helper").add_desc
 vim.api.nvim_create_augroup("NullLSMaps", { clear = true })
 vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
-    pattern = { "*.sh" },
+    pattern = { "*.sh", ".bashrc", "*.sql" },
     callback = function(ev)
         local leader_temp = vim.g.mapleader
         local bufopts = { noremap = true, silent = true, buffer = ev.buf }
         vim.g.mapleader = ' '
+        --print("null-ls setting keymaps ...")
         -- Start keymaps
         vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format({ timeout_ms = 2000 }) end,
             add_desc(bufopts, "Format code."))
@@ -31,6 +37,11 @@ vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
             add_desc(bufopts, "Code action."))
         vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action,
             add_desc(bufopts, "Code action."))
+        vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, add_desc(bufopts, 'Show diagnostics in float.'))
+        vim.keymap.set('n', '<leader>l', vim.diagnostic.setloclist,
+            add_desc(bufopts, 'Send diagnostics to location list.'))
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, add_desc(bufopts, 'Previous diagnostic.'))
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, add_desc(bufopts, 'Next diagnostic.'))
         -- End keymaps
         vim.g.mapleader = leader_temp
     end,
