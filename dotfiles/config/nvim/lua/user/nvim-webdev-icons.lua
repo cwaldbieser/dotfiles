6f45b67d00b664784573e656aa96d55669d896c1
Carl Waldbieser <cwaldbieser@gmail.com>
2023-07-14 18:13:19 -0400

    [nvim] Set custom icon for CSV file
    [nvim] Set custom icon for CSV file

--- dotfiles/config/nvim/lua/user/nvim-webdev-icons.lua
+++ dotfiles/config/nvim/lua/user/nvim-webdev-icons.lua
@@ -1,5 +1,11 @@
 require 'nvim-web-devicons'.setup {
     override = {
+        csv = {
+            icon = "",
+            color = "#89e051",
+            cterm_color = "113",
+            name = "Csv",
+        },
         xml = {
             icon = "",
             color = "#e37933",
