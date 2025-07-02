cae725d84cd8e9d33e5102f205060de941e08604
Carl Waldbieser <cwaldbieser@gmail.com>
2025-07-02 09:26:27 -0400

    [chore] noice integration with lualine for recording macros

--- dotfiles/config/nvim/lua/user/lualine.lua
+++ dotfiles/config/nvim/lua/user/lualine.lua
@@ -3,37 +3,44 @@
 -- =====================
 
 -- Include icons.
-require('nvim-web-devicons').setup()
+require("nvim-web-devicons").setup()
 
 -- lualine configuration
-require('lualine').setup({
-    options = {
-        -- icons_enabled=false,
-        theme = 'auto',
-        component_separators = { left = '|', right = '|' },
-        section_separators = { left = '', right = '' },
-    },
-    sections = {
-        lualine_b = {
-            "branch", "diff",
-            {
-                "diagnostics",
-                symbols = { error = '', warn = '', info = '', hint = '🔍' },
-            }
-        },
-        lualine_x = {
-            {
-                'encoding',
-                -- icons_enabled=false
-            },
-            {
-                'fileformat',
-                icons_enabled = false
-            },
-            {
-                'filetype',
-                -- icons_enabled=false
-            }
-        }
-    }
+require("lualine").setup({
+	options = {
+		-- icons_enabled=false,
+		theme = "auto",
+		component_separators = { left = "|", right = "|" },
+		section_separators = { left = "", right = "" },
+	},
+	sections = {
+		lualine_b = {
+			"branch",
+			"diff",
+			{
+				"diagnostics",
+				symbols = { error = "", warn = "", info = "", hint = "🔍" },
+			},
+		},
+		lualine_x = {
+			{
+				"encoding",
+				-- icons_enabled=false
+			},
+			{
+				"fileformat",
+				icons_enabled = false,
+			},
+			{
+				"filetype",
+				-- icons_enabled=false
+			},
+			-- noice integration for macro recording.
+			{
+				require("noice").api.statusline.mode.get,
+				cond = require("noice").api.statusline.mode.has,
+				color = { fg = "#ff9e64" },
+			},
+		},
+	},
 })
